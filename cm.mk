c533839ab359e23b04003a201bafd373a4655090
Steve Kondik <shade@chemlab.org>
2012-06-17 18:22:21 -0700

    quincyatt: Enable enhanced NFC
    Change-Id: I8e3724280466271b02a827975512db5b9060fccc

--- cm.mk
+++ cm.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/samsung/quincyatt/full_quincyatt.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
