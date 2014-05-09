f28fc96d337c3c93dfabc5742d2a363d1134f1e5
black <blackzigong@gmail.com>
2014-05-09 04:46:42 -0700

    quincyatt: Remove gsm.mk
    Change-Id: Ia46bc6f98c1110cea4f96988f2e1a75c4724f57d

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/quincyatt/full_quincyatt.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
