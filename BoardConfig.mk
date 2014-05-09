f2190e88f64c381c0e454e803ba982407601e15d
black <blackzigong@gmail.com>
2014-05-09 10:15:16 -0700

    quincyatt: Move external storage definition to device specific tree
    quincyatt's secondary sdcard is /dev/block/mmcblk0p28 but quincydcm's is /dev/block/mmcblk0p29.
    It need to be defined in each device specific tree.
    
    Change-Id: Ie845d67dec1060b5940da25f12e18804c4a72ee3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,4 +47,5 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/quincyatt/bluetooth
 
-
+# secondary sdcard
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0p28
